7a7a87523a8dc82cba527b7c10da42514daea0e0
mantissss <sebesteanattilo@yahoo.com>
2022-01-16 12:52:45 +0200

    Lab12

---
+++ Sebestean_Balazs_Attila_Lab12/Sebestean_Balazs_Attila_Lab12/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Sebestean_Balazs_Attila_Lab12.Data;
+
+namespace Sebestean_Balazs_Attila_Lab12
+{
+    public partial class App : Application
+    {
+
+        public static ShoppingListDatabase Database { get; private set; }
+        public App()
+        {
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+            //InitializeComponent();
+
+            //MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Sebestean_Balazs_Attila_Lab12/Sebestean_Balazs_Attila_Lab12/Data/IRestService .cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Sebestean_Balazs_Attila_Lab12.Models;
+
+namespace Sebestean_Balazs_Attila_Lab12.Data
+{
+    public interface IRestService
+    {
+        Task<List<ShopList>> RefreshDataAsync();
+        Task SaveShopListAsync(ShopList item, bool isNewItem);
+        Task DeleteShopListAsync(int id);
+    }
+}
